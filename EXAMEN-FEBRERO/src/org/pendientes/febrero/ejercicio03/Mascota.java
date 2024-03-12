4a0e3bc5476de3816dec40a203e26f7bc9ed0b43
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2024-03-12 15:06:52 +0100

    Continuación examen febrero

--- EXAMEN-FEBRERO/src/org/pendientes/febrero/ejercicio03/Mascota.java
+++ EXAMEN-FEBRERO/src/org/pendientes/febrero/ejercicio03/Mascota.java
@@ -1,6 +1,8 @@
 package org.pendientes.febrero.ejercicio03;
 
-public class Mascota {
+import java.util.Objects;
+
+public class Mascota implements Comparable<Mascota>  {
 	private final int id;
 	private final String nombre;
 	private final String propietario;
@@ -40,6 +42,67 @@ public class Mascota {
 		return "Mascota [id=" + id + ", nombre=" + nombre + ", propietario=" + propietario + ", edadMeses=" + edadMeses
 				+ ", pesoKg=" + pesoKg + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Mascota other = (Mascota) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public int compareTo(Mascota that) {
+		// Comparar this con that
+		// Si this va antes en el orden natural que taht, devolvemos < 0 (-1)
+		// Si this y that van a la par (en el mismo sitio en el orden natural) devolver 0
+		// Si this va después en el orden natural que that, devolvemos > 0 (1)
+
+		// Forma "rústica"
+//		if (this.id < that.id) {
+//			return -1;
+//		}
+//		if (this.id > that.id) {
+//			return 1;
+//		}
+//		return 0;
+		
+		return Integer.compare(this.id, that.id);
+		
+		// Si el orden natural es de mayot a menor hay dos opciones
+		// Opcion 1: invertir el resultado
+		//return -Integer.compare(this.id, that.id);
+		// Opción 2: invertir el orden de los parámetros
+		//return Integer.compare(that.id, this.id);		
+		
+	}
+
+//	@Override
+//	public int hashCode() {
+//		return Objects.hash(id);
+//	}
+//
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (!(obj instanceof Mascota))
+//			return false;
+//		Mascota other = (Mascota) obj;
+//		return id == other.id;
+//	}
+	
+	
+	
 	
 	
 }
--- EXAMEN-FEBRERO/src/org/pendientes/febrero/ejercicio03/Programa.java
+++ EXAMEN-FEBRERO/src/org/pendientes/febrero/ejercicio03/Programa.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,12 +17,51 @@ public class Programa {
 		List<Mascota> mascotas = leerMacotas(PATH_FICHERO);
 		
 		// Esto es con programación funcional y referencia a métodos
-		mascotas.forEach(System.out::println);
+		//  mascotas.forEach(System.out::println);
 		
 		// con un for-each tradicional:
 		for (Mascota mascota : mascotas) {
 			System.out.println(mascota);
 		}
+		
+		System.out.println("-".repeat(100));
+		
+		// Ordenar en orden natural
+		mascotas.sort(null);
+	
+		for (Mascota mascota : mascotas) {
+			System.out.println(mascota);
+		}
+		
+		System.out.println("-".repeat(100));
+		
+		// Ordenar al reves del orden natural
+		mascotas.sort(Comparator.reverseOrder());
+	
+		for (Mascota mascota : mascotas) {
+			System.out.println(mascota);
+		}
+
+		System.out.println("-".repeat(100));
+
+		// Ordenar por nombre
+		mascotas.sort(Comparator.comparing(Mascota::getNombre));
+	
+		for (Mascota mascota : mascotas) {
+			System.out.println(mascota);
+		}
+		System.out.println("-".repeat(100));
+
+		// Ordenar por nombre y si son iguales los nombres, por id
+		mascotas.sort(Comparator.comparing(Mascota::getNombre).thenComparing(Comparator.naturalOrder()));
+	
+		for (Mascota mascota : mascotas) {
+			System.out.println(mascota);
+		}
+
+		
+		
+//		Collections.sort(mascotas);
 		
 	}
 	
